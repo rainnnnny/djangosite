11f9e53ca4759a58350168a0a5677570480fcc0a
rainnnnny <rainnnnny@sina.com>
2017-08-28 23:55:53 +0800

    first commit

---
+++ hellodjango/urls.py
@@ -0,0 +1,25 @@
+"""hellodjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from helloapp import views as helloviews
+
+urlpatterns = [
+	url(r'^$', helloviews.hello),
+	url(r'^add/$', helloviews.add, name='add'),
+	url(r'^add/(\d+)/(\d+)/$', helloviews.add2, name='add2'),
+    url(r'^admin/', admin.site.urls),
+]
