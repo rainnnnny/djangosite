2ab3fdcb5c84ddd66d6b9c2b4abcdae3f4e8502e
rainnnnny <rainnnnny@sina.com>
2017-09-29 10:37:50 +0800

    decorator

--- everything.py
+++ everything.py
@@ -1,29 +1,69 @@
 import xlrd
 import os
+import random
+import re
+import time
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+import django
+django.setup()
 
 '''
 Django 版本大于等于1.7的时候，需要加上下面两句
-
-否则会抛出错误 django.core.exceptions.AppRegistryNotReady: Models aren't loaded yet.
-'''
 import django
 django.setup()
+否则会抛出错误 django.core.exceptions.AppRegistryNotReady: Models aren't loaded yet.
+'''
+
 from main.models import Express
 import django.utils.timezone as timezone
-import random
-import re
 
-all = Express.objects.all().values()
-for each in all:
-    sid = each['shelfID']
-    if not re.match("[a-z][0-9]{4}", sid):
-        a  = random.randint(97, 122)
-        c = chr(a)
-        b = random.randint(1000, 9999)
-        s = "%s%s"%(c,b)
-        o = Express.objects.get(id=each['id'])
-        o.shelfID=s
-        o.save()
-        print(s)
+
+oAll = Express.objects.all()
+
+
+def deco(func):
+    def wrapper(a, b):
+        st = time.time()
+        func(a, b)
+        et = time.time()
+        print("run time:%s"%(st-et))
+    return wrapper
+
+@deco
+def add(a, b):
+    print(a+b)
+
+add(1, 2)
+
+
+s = "string in global"
+num = 99
+
+def numFunc(a, b):
+    num = 100
+    s = "string in addFunc"
+    print( "print s in numFunc: ", s)
+    print( "print num in numFunc: ", num)
+
+    def addFunc(a, b):
+        print( "print s in addFunc: ", s)
+        print( "print num in addFunc: ", num)
+        return "%d + %d = %d" %(a, b, a + b)
+
+    return addFunc(a, b)
+
+print(numFunc(3, 6))
+
+
+
+def singleton(cls, *args):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls(*args)
+        return instances[cls]
+    return getinstance
+
+@singleton
+class Myclass():
