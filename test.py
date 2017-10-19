e1f6f842a1b4c1c1c92fc0c1f6237d3e3748f3c9
rainnnnny <rainnnnny@sina.com>
2017-10-19 18:16:25 +0800

    1

---
+++ test.py
@@ -0,0 +1,17 @@
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+# import django
+# django.setup()
+
+# '''
+# Django 版本大于等于1.7的时候，需要加上下面两句
+# import django
+# django.setup()
+# 否则会抛出错误 django.core.exceptions.AppRegistryNotReady: Models aren't loaded yet.
+# '''
+
+# from main.models import Express
+# import django.utils.timezone as timezone
+
+
+# oAll = Express.objects.all()
